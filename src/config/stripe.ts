f2b8b3577734a42bae90cfded8f84ebd7640089f
JOBOYA <lena.boya26@gmail.com>
2023-10-29 18:06:08 +0100

    en

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -1,6 +1,6 @@
 export const PLANS = [
   {
-    name: 'Gratuit',
+    name: 'Free',
     slug: 'free',
     quota: 10,
     pagesPerPdf: 5,
@@ -21,7 +21,7 @@ export const PLANS = [
       amount: 8,
       priceIds: {
         test: 'price_1O6adUG2lyYn0GZu971gXBe1',
-        production: 'price_1O6adUG2lyYn0GZu971gXBe1',
+        production: '',
       },
     },
   },
