9ba4724886fbbec5a06ff33d9c575ecb41d81ebb
JOBOYA <lena.boya26@gmail.com>
2023-10-30 11:47:59 +0100

    test5

--- src/components/BillingForm.tsx
+++ src/components/BillingForm.tsx
@@ -1,77 +1,89 @@
-import { getUserSubscriptionPlan } from '@/lib/stripe';
-import { useToast } from './ui/use-toast';
-import { trpc } from '@/app/_trpc/client';
-import MaxWidthWrapper from './MaxWidthWrapper';
+'use client'
+
+import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { useToast } from './ui/use-toast'
+import { trpc } from '@/app/_trpc/client'
+import MaxWidthWrapper from './MaxWidthWrapper'
 import {
   Card,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
-} from './ui/card';
-import { Button } from './ui/button';
-import { Loader2 } from 'lucide-react';
-import { format } from 'date-fns';
+} from './ui/card'
+import { Button } from './ui/button'
+import { Loader2 } from 'lucide-react'
+import { format } from 'date-fns'
 
 interface BillingFormProps {
   subscriptionPlan: Awaited<
     ReturnType<typeof getUserSubscriptionPlan>
-  >;
+  >
 }
 
-const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
-  const { toast } = useToast();
-  const { mutate: createStripeSession, isLoading } = trpc.createStripeSession.useMutation({
-    onSuccess: ({ url }) => {
-      if (url) window.location.href = url;
-      if (!url) {
-        toast({
-          title: 'Il y a un problème...',
-          description: 'Veuillez réessayer dans un instant',
-          variant: 'destructive',
-        });
-      }
-    },
-  });
+const BillingForm = ({
+  subscriptionPlan,
+}: BillingFormProps) => {
+  const { toast } = useToast()
 
-  const handleButtonClick = () => {
-    if (subscriptionPlan.isSubscribed) {
-      window.location.href = 'https://billing.stripe.com/p/login/00g8x42sy1p2c8g5kk';
-    } else {
-      createStripeSession();
-    }
-  };
+  const { mutate: createStripeSession, isLoading } =
+    trpc.createStripeSession.useMutation({
+      onSuccess: ({ url }) => {
+        if (url) window.location.href = url
+        if (!url) {
+          toast({
+            title: 'There was a problem...',
+            description: 'Please try again in a moment',
+            variant: 'destructive',
+          })
+        }
+      },
+    })
 
   return (
-    <MaxWidthWrapper>
-      <form className='mt-12'>
+    <MaxWidthWrapper className='max-w-5xl'>
+      <form
+        className='mt-12'
+        onSubmit={(e) => {
+          e.preventDefault()
+          createStripeSession()
+        }}>
         <Card>
           <CardHeader>
-            <CardTitle>Subscription Plan</CardTitle>
+            <CardTitle>Plan de souscription</CardTitle>
             <CardDescription>
-              Vous êtes actuellement sur le <strong>{subscriptionPlan.name}</strong> plan.
+              Vous êtes actuellement sur le{' '}
+              <strong>{subscriptionPlan.name}</strong> plan.
             </CardDescription>
           </CardHeader>
+
           <CardFooter className='flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0'>
-            <Button type='button' onClick={handleButtonClick}>
+            <Button type='submit'>
               {isLoading ? (
                 <Loader2 className='mr-4 h-4 w-4 animate-spin' />
               ) : null}
-              {subscriptionPlan.isSubscribed ? 'Gérer mon abonnement' : 'Passer à Pro'}
+              {subscriptionPlan.isSubscribed
+                ? 'Gérez mon abonnement'
+                : 'Mise à niveau PRO'}
             </Button>
+
             {subscriptionPlan.isSubscribed ? (
               <p className='rounded-full text-xs font-medium'>
                 {subscriptionPlan.isCanceled
-                  ? 'Votre forfait sera annulé le '
-                  : 'Votre forfait se renouvelle le '}
-                {format(subscriptionPlan.stripeCurrentPeriodEnd!, 'dd.MM.yyyy')}.
+                  ? 'Votre plan  expire le '
+                  : 'Votre plan se renouvelle le'}
+                {format(
+                  subscriptionPlan.stripeCurrentPeriodEnd!,
+                  'dd.MM.yyyy'
+                )}
+                .
               </p>
             ) : null}
           </CardFooter>
         </Card>
       </form>
     </MaxWidthWrapper>
-  );
-};
+  )
+}
 
-export default BillingForm;
+export default BillingForm
